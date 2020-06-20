5efe825fa1e3a747a30fa02e43c55a9d668ec31d
Vad-Springboard <67168204+Vad-Springboard@users.noreply.github.com>
2020-06-19 23:06:39 -0500

    Add files via upload

---
+++ appendToString.js
@@ -0,0 +1,37 @@
+// Write a function called appendToString, which accepts two strings.
+
+// The function should return a new string which consists of the second string appended to the first string.
+
+// Examples:
+
+// appendToString("Hello", " World!"); // "Hello World!"
+
+// appendToString("Foo", "bar"); // "Foobar"
+
+// appendToString("bar", "Foo"); // "barFoo"
+
+// appendToString("", "test"); // "test"
+
+// appendToString("other test", ""); // "other test"
+
+// Here is my solution:
+
+function appendToString(str1, str2) {
+	console.log(str1 + str2);
+}
+
+// appendToString('Hello', ' World!');
+// appendToString('Foo', 'bar');
+// appendToString('bar', 'Foo');
+// appendToString('', 'test');
+// appendToString('other test', '');
+
+// in Chrome console I'm getting concatenated string OK, however repl does not accept it.
+
+// this is a note from repl.it:
+// Failed tests
+// returns_a_string_with_appended_characters (this requirement apparently is not met. Vadym)
+// Make sure you return a new string with the new characters appended to the original string
+// Expected undefined to be 'Hello World!'.
+
+// I've added another file to github with similar problem, prependToString -works in console; repl does not like it.
